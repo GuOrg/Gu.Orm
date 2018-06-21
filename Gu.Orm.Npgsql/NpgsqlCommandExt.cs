7e175c824b2b26950d233f86ea3cdd654ce72735
Johan Larsson <johan_gavle1@hotmail.com>
2018-06-21 12:59:23 +0200

    Initial.

---
+++ Gu.Orm.Npgsql/NpgsqlCommandExt.cs
@@ -0,0 +1,30 @@
+﻿namespace Gu.Orm.Npgsql
+{
+    using System;
+    using System.Data;
+    using global::Npgsql;
+
+    public static class NpgsqlCommandExt
+    {
+        [Obsolete("This is for triggering the analyzer to create the func.", error: true)]
+        public static bool TrySingle<T>(this NpgsqlCommand command, out T result)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static bool TrySingle<T>(this NpgsqlCommand command, Func<NpgsqlDataReader, T> read, out T result)
+        {
+            using (var reader = command.ExecuteReader(CommandBehavior.SingleRow))
+            {
+                if (reader.Read())
+                {
+                    result = read(reader);
+                    return true;
+                }
+            }
+
+            result = default(T);
+            return false;
+        }
+    }
+}
---
+++ Gu.Orm.Npgsql/NpgsqlConnectionExt.cs
@@ -0,0 +1,27 @@
+﻿namespace Gu.Orm.Npgsql
+{
+    using global::Npgsql;
+
+    public static class NpgsqlConnectionExt
+    {
+        public static NpgsqlCommand CreateCommand(this NpgsqlConnection connection, string sql)
+        {
+            return new NpgsqlCommand(sql, connection);
+        }
+
+        public static NpgsqlCommand CreateCommand(this NpgsqlConnection connection, string sql, NpgsqlParameter parameter)
+        {
+            var command = new NpgsqlCommand(sql, connection);
+            command.Parameters.Add(parameter);
+            return command;
+        }
+
+        public static NpgsqlCommand CreateCommand(this NpgsqlConnection connection, string sql, NpgsqlParameter parameter1, NpgsqlParameter parameter2)
+        {
+            var command = new NpgsqlCommand(sql, connection);
+            command.Parameters.Add(parameter1);
+            command.Parameters.Add(parameter2);
+            return command;
+        }
+    }
+}
