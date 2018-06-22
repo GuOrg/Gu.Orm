6320f28a761b7f984ef55d36d1aa4b65c3cf6b17
Johan Larsson <johan_gavle1@hotmail.com>
2018-06-22 03:19:00 +0200

    Dump table schema

---
+++ Gu.Orm.Npgsql.Tests/Sandbox.cs
@@ -0,0 +1,53 @@
+﻿namespace Gu.Orm.Npgsql.Tests
+{
+    using System;
+    using System.Configuration;
+    using global::Npgsql;
+    using NUnit.Framework;
+
+    public class Sandbox
+    {
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            using (var db = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["Database"].ConnectionString))
+            {
+                db.Open();
+                var command = db.CreateCommand(@"
+DROP TABLE IF EXISTS foos;
+
+CREATE TABLE foos(
+  id SERIAL PRIMARY KEY NOT NULL,
+  text VARCHAR(200));
+
+INSERT INTO foos(text) VALUES 
+  ('1'),
+  ('2'),
+  ('3'),
+  ('4');
+");
+                command.ExecuteNonQuery();
+            }
+        }
+
+        [Test]
+        public void Map()
+        {
+            using (var db = new NpgsqlConnection(ConfigurationManager.ConnectionStrings["Database"].ConnectionString))
+            {
+                db.Open();
+                using (var command = db.CreateCommand(@"SELECT * FROM foos
+                                                        WHERE false"))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        foreach (var column in reader.GetColumnSchema())
+                        {
+                            Console.WriteLine($"Name: {column.ColumnName}, Type: {column.DataType}");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
